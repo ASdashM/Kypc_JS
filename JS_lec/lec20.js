c9694eda2de1ad7c9ef42a4dfee67c51c06ecf8e
ASdashM <danilput19@gmail.com>
2022-06-23 12:36:10 +0300

    lec22

--- JS_lec/lec20.js
+++ JS_lec/lec20.js
@@ -1,3 +1,4 @@
+"use strict";
 let num = 21;
 
 if (num == 20) {            //Условия
---
+++ JS_lec/lec22.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// Цикл WHILE
+
+let num = 21;
+while (num < 30) {
+    console.log(num);
+    num++;
+ }
+
+ //Цикл DO
+
+let num2 = 10;
+
+do {
+    console.log(num2);
+    num2++;
+}
+while (num2 < 15);
+
+// Обычный цикл FOR
+
+for (let i = 1; i < 10; i++) {
+    console.log(i);
+}
+
+//Цикл FOR с переменной let
+
+let num3 = 20;
+
+for(let i = 1; i < 10; i++) {
+    console.log(num3);
+     num3++;
+}
